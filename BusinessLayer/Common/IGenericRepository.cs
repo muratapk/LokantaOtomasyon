88351e4b206a1a61f2dcd97d8dfb16d70c7f3b46
murat ciplak <muratapk@hotmail.com>
2023-12-17 23:03:26 +0300

    GenericRepository eklendi

---
+++ BusinessLayer/Common/IGenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Common
+{
+    public interface IGenericRepository<T>where T:class
+    {
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        // void GetAll();
+        //void GetId(int id);
+        IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null);
+        T GetId(Expression<Func<T, bool>> filter, string? includeProperties = null);
+        void Save();
+    }
+}
---
+++ DataAccessLayer/Repository/GenericRepository.cs
@@ -0,0 +1,80 @@
+﻿using BusinessLayer.Common;
+using DataAccessLayer.Data;
+using EntityLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(T entity)
+        {
+            _context.Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Remove(entity);
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null)
+        {
+            IQueryable<T> sorgu = _context.Set<T>();
+            if (filter != null)
+            {
+                sorgu = sorgu.Where(filter);
+            }
+            if (string.IsNullOrEmpty(includeProperties))
+            {
+                foreach (var includeprop in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    sorgu = sorgu.Include(includeprop);
+                }
+
+            }
+            return sorgu.ToList();
+        }
+
+        public T GetId(Expression<Func<T, bool>> filter, string? includeProperties = null)
+        {
+            IQueryable<T> sorgu = _context.Set<T>();
+            if (filter != null)
+            {
+                sorgu = sorgu.Where(filter);
+            }
+            if (string.IsNullOrEmpty(includeProperties))
+            {
+                foreach (var includeprop in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    sorgu = sorgu.Include(includeprop);
+                }
+
+            }
+            return sorgu.FirstOrDefault();
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+           _context.Update(entity);
+        }
+    }
+}
