5f287b3a6c042de5e3aac0a08ef89e71100daf05
muratapk <muratapk@hotmail.com>
2023-12-24 15:49:42 +0300

    Adisyonlar Eklendi

---
+++ LokantaOtomasyon/Controllers/AdisyonController.cs
@@ -0,0 +1,62 @@
+﻿using BusinessLayer.Common;
+using EntityLayer.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LokantaOtomasyon.Controllers
+{
+    public class AdisyonController : Controller
+    {
+        private readonly IAdisyonRepository _repository;
+
+        public AdisyonController(IAdisyonRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IActionResult Index()
+        {
+            var rollerim = _repository.GetAll();
+            return View(rollerim);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Adisyon gelen)
+        {
+            _repository.Add(gelen);
+            _repository.Save();
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var value = _repository.GetId(u => u.Adisyon_Id == id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult Edit(Adisyon gelen)
+        {
+            _repository.Update(gelen);
+            _repository.Save();
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            _repository.GetId(u => u.Adisyon_Id == id);
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Delete(Adisyon gelen)
+        {
+            _repository.Delete(gelen);
+            _repository.Save();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
+
---
+++ LokantaOtomasyon/Controllers/SiparislerController.cs
@@ -0,0 +1,62 @@
+﻿using BusinessLayer.Common;
+using EntityLayer.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LokantaOtomasyon.Controllers
+{
+    public class ServislerController : Controller
+    {
+        private readonly IServislerRepository _repository;
+
+        public ServislerController(IServislerRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IActionResult Index()
+        {
+            var rollerim = _repository.GetAll();
+            return View(rollerim);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Servisler gelen)
+        {
+            _repository.Add(gelen);
+            _repository.Save();
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var value = _repository.GetId(u => u.Servis_Id == id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult Edit(Servisler gelen)
+        {
+            _repository.Update(gelen);
+            _repository.Save();
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            _repository.GetId(u => u.Servis_Id == id);
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Delete(Servisler gelen)
+        {
+            _repository.Delete(gelen);
+            _repository.Save();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
+
