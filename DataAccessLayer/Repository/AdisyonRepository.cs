66c372b20080c2198ff17c36ae179ba470bf5d81
muratapk <muratapk@hotmail.com>
2023-12-24 15:35:32 +0300

    Adisyon ve diğer projelerin eklemesi

---
+++ DataAccessLayer/Repository/AdisyonRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Common;
+using DataAccessLayer.Data;
+using EntityLayer.Entities;
+namespace DataAccessLayer.Repository
+{
+    public class AdisyonRepository:GenericRepository<Adisyon>,IAdisyonRepository
+    {
+        public readonly ApplicationDbContext _context;
+
+        public AdisyonRepository(ApplicationDbContext context):base(context)
+        {
+            _context = context;
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ DataAccessLayer/Repository/SiparislerRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Common;
+using DataAccessLayer.Data;
+using EntityLayer.Entities;
+namespace DataAccessLayer.Repository
+{
+    public class SiparislerRepository:GenericRepository<Siparisler>,ISiparislerRepository
+    {
+        public readonly ApplicationDbContext _context;
+
+        public SiparislerRepository(ApplicationDbContext context):base(context)
+        {
+            _context = context;
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
